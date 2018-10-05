6a3f917e7454ecd535ca40fe926cfb4a4aa85812
tdietert <thomas@adjoint.io>
2018-10-05 09:44:07 +0100

    More sketching of effect layer for Raft

--- src/Raft.hs
+++ src/Raft.hs
@@ -14,11 +14,14 @@ module Raft where
 import Protolude hiding (Chan, newChan, readChan, writeChan, STM)
 
 import Control.Concurrent.Classy
+import Control.Concurrent.Classy.Async
 import Control.Concurrent.Classy.STM
 import Control.Monad.Catch
 import Control.Concurrent.STM.Timer
 
 import Control.Monad.Trans.Class
+
+import qualified Data.Map as Map
 
 import Numeric.Natural
 
@@ -26,6 +29,7 @@ import Raft.Action
 import Raft.Config
 import Raft.Event
 import Raft.Handle
+import Raft.Log
 import Raft.Monad
 import Raft.NodeState
 import Raft.Persistent
@@ -35,46 +39,72 @@ import Raft.Types
 
 -- | Provide an interface for nodes to send/receive messages to/from one
 -- another. E.g. Control.Concurrent.Chan, Network.Socket, etc.
-class MonadConc m => RaftRPC m v where
+class MonadConc m => RaftSendRPC m v where
   sendRPC :: NodeId -> Message v -> m ()
+
+class MonadConc m => RaftRecvRPC m v where
   receiveRPC :: m (Message v)
-  broadcastRPC :: NodeIds -> Message v -> m ()
 
---- | Functional dependency allowing only one v per sm
-class RaftStateMachine sm v | sm -> v where
-  applyLogEntry :: sm -> v -> sm
+--- | The underlying raft state machin. Functional dependency permitting only
+--a single state machine command to be defined to update the state machine.
+class RaftStateMachine sm cmd | sm -> cmd where
+  applyCommittedLogEntry :: sm -> cmd -> sm
 
+-- | A typeclass providing an interface to save and load the persistent state of
+-- a raft node.
+--
+-- TODO: It is infeasible to keep the persistent state in memory, as it contains
+-- all of the logs in their entirety. Perhaps there is a better solution... see
+-- the Raft.PersistentState module.
 class MonadConc m => RaftPersist m v where
   savePersistentState :: PersistentState v -> m ()
   loadPersistentState :: m (PersistentState v)
 
-data RaftState s m v = RaftState
+-- | The pure state of the raft node
+--
+-- TODO: Will probably have to store the state machine in a [write only (?)]
+-- concurrent variable, as the client program using this raft implementation
+-- will need the ability to read the current state of the state machine at
+-- arbitrary times.
+data RaftState s v = RaftState
   { serverNodeState :: RaftNodeState v
   , serverStateMachine :: s
-  , serverEventChan :: Chan m (Event v)
   }
 
-newtype RaftStateM s v m a = RaftStateM
-  { unRaftStateM :: ReaderT (RaftState s m v) m a
-  } deriving (Functor, Applicative, Monad, MonadReader (RaftState s m v), Alternative, MonadPlus)
+-- | The raft server environment composed of the concurrent variables used in
+-- the effectful raft layer.
+data RaftEnv m v = RaftEnv
+  { serverEventChan :: Chan m (Event v)
+  , serverElectionTimer :: Timer m
+  , serverHeartbeatTimer :: Timer m
+  }
 
-instance MonadTrans (RaftStateM s m) where
-  lift = RaftStateM . lift
+newtype RaftT s v m a = RaftT
+  { unRaftT :: ReaderT (RaftEnv m v) (StateT (RaftState s v) m) a
+  } deriving (Functor, Applicative, Monad, MonadReader (RaftEnv m v), MonadState (RaftState s v), Alternative, MonadPlus)
 
-deriving instance MonadThrow m => MonadThrow (RaftStateM s v m)
-deriving instance MonadCatch m => MonadCatch (RaftStateM s v m)
+instance MonadTrans (RaftT s m) where
+  lift = RaftT . lift . lift
 
-deriving instance MonadSTM m => MonadSTM (RaftStateM s v m)
-deriving instance MonadMask m => MonadMask (RaftStateM s v m)
-deriving instance MonadConc m => MonadConc (RaftStateM s v m)
+deriving instance MonadThrow m => MonadThrow (RaftT s v m)
+deriving instance MonadCatch m => MonadCatch (RaftT s v m)
+deriving instance MonadSTM m => MonadSTM (RaftT s v m)
+deriving instance MonadMask m => MonadMask (RaftT s v m)
+deriving instance MonadConc m => MonadConc (RaftT s v m)
 
-runRaftStateM :: MonadConc m => RaftState s m v -> RaftStateM s v m () -> m ()
-runRaftStateM raftState = flip runReaderT raftState . unRaftStateM
+runRaftT
+  :: MonadConc m
+  => RaftState s v
+  -> RaftEnv m v
+  -> RaftT s v m ()
+  -> m ()
+runRaftT raftState raftEnv =
+  flip evalStateT raftState . flip runReaderT raftEnv . unRaftT
 
 ------------------------------------------------------------------------------
 
 runRaftNode
-   :: (MonadConc m, RaftStateMachine s v, RaftRPC m v, RaftPersist m v)
+   :: (MonadConc m, RaftStateMachine s v, RaftSendRPC m v, RaftRecvRPC m v, RaftPersist m v)
    => NodeConfig
    -> s
    -> m ()
@@ -87,38 +117,70 @@ runRaftNode nodeConfig@NodeConfig{..} initStateMachine = do
   fork (heartbeatTimeoutTimer configElectionTimeout electionTimer eventChan)
   fork (rpcHandler eventChan)
 
-  let raftState = RaftState initRaftNodeState initStateMachine eventChan
-  runRaftStateM raftState (handleEventLoop nodeConfig)
+  let raftState = RaftState initRaftNodeState initStateMachine
+      raftEnv = RaftEnv eventChan electionTimer heartbeatTimer
+  runRaftT raftState raftEnv (handleEventLoop nodeConfig)
 
 handleEventLoop
-   :: forall v m s. (MonadConc m, RaftStateMachine s v, RaftPersist m v, RaftRPC m v)
+   :: forall v m s. (MonadConc m, RaftStateMachine s v, RaftPersist m v, RaftSendRPC m v)
    => NodeConfig
-   -> RaftStateM s v m ()
+   -> RaftT s v m ()
 handleEventLoop nodeConfig =
     handleEventLoop' =<< lift loadPersistentState
   where
-    handleEventLoop' :: PersistentState v -> RaftStateM s v m ()
+    handleEventLoop' :: PersistentState v -> RaftT s v m ()
     handleEventLoop' persistentState = do
-      eventChan <- asks serverEventChan
-      event <- lift (readChan eventChan)
+      event <- lift . readChan =<< asks serverEventChan
       let (resRaftNodeState, resPersistentState, actions) =
             Raft.Handle.handleEvent nodeConfig initRaftNodeState persistentState event
       lift $ savePersistentState resPersistentState
+      updateRaftNodeState resRaftNodeState
       handleActions actions
       handleEventLoop' resPersistentState
 
-handleActions :: (MonadConc m, RaftStateMachine s v, RaftRPC m v) => [Action v] -> RaftStateM s v m ()
+    updateRaftNodeState :: RaftNodeState v -> RaftT s v m ()
+    updateRaftNodeState newRaftNodeState =
+      modify $ \raftState ->
+        raftState { serverNodeState = newRaftNodeState }
+
+handleActions
+  :: (MonadConc m, RaftStateMachine s v, RaftSendRPC m v)
+  => [Action v]
+  -> RaftT s v m ()
 handleActions = mapM_ handleAction
 
-handleAction :: (MonadConc m, RaftStateMachine s v, RaftRPC m v) => Action v -> RaftStateM s v m ()
-handleAction action = undefined
-
+handleAction
+  :: (MonadConc m, RaftStateMachine s v, RaftSendRPC m v)
+  => Action v
+  -> RaftT s v m ()
+handleAction action =
+  case action of
+    SendMessage nid msg -> lift (sendRPC nid msg)
+    SendMessages msgs ->
+      forConcurrently_ (Map.toList msgs) $ \(nid, msg) ->
+        lift (sendRPC nid msg)
+    Broadcast nids msg ->
+      mapConcurrently_ (lift . flip sendRPC msg) nids
+    RedirectClient (ClientId cid) currLdr -> undefined
+    RespondToClient cid -> undefined
+    ApplyCommittedLogEntry entry -> do
+      modify $ \raftState -> do
+        let stateMachine = serverStateMachine raftState
+            v = entryValue entry
+        raftState { serverStateMachine = applyCommittedLogEntry stateMachine v }
+    ResetTimeoutTimer tout n -> do
+      case tout of
+        ElectionTimeout -> resetServerTimer serverElectionTimer n
+        HeartbeatTimeout -> resetServerTimer serverHeartbeatTimer n
+  where
+    resetServerTimer f n =
+      lift . resetTimer n =<< asks f
 ------------------------------------------------------------------------------
  --Event Producers
 ------------------------------------------------------------------------------
 
 -- | Producer for rpc message events
-rpcHandler :: (MonadConc m, RaftRPC m v) => Chan m (Event v) -> m ()
+rpcHandler :: (MonadConc m, RaftRecvRPC m v) => Chan m (Event v) -> m ()
 rpcHandler eventChan =
   forever $
     receiveRPC >>= \rpcMsg ->
@@ -138,4 +200,3 @@ heartbeatTimeoutTimer n timer eventChan =
   forever $ do
     startTimer n timer >> waitTimer timer
     writeChan eventChan (Timeout HeartbeatTimeout)
-
